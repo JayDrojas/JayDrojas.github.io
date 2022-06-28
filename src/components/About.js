af8d5dd19bf882d42c9ae09d519d84c5927adb55
Damian Rojas <86801740+JayDrojas@users.noreply.github.com>
2022-06-28 15:36:04 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -85,7 +85,7 @@ class About extends Component {
                     <br />
                     <span> Email: damianxrojas@gmail.com</span>
                     <br />
-                    <span><a href="https://drive.google.com/file/d/1vEzSM-0kZo4lcS8Bo7LqjyxZWXr_yX4A/view?usp=sharing">View my resume</a></span>
+                    <span><a href="https://drive.google.com/file/d/1Uk5-HFVfcCr9CdV29r-qQ2tIMlB5a5My/view?usp=sharing">View my resume</a></span>
                   </div>
                 </div>
               </div>
